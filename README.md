b82e3e7b82242187c8fba5769ca04a40a0629d36
damesek <sz@baader.hu>
2019-10-14 12:01:00 +0200

    mini example

---
+++ README.md
@@ -0,0 +1,32 @@
+# A MiniMe example of reagent + shadow-cljs + "Oz" setup
+
+I had issues to integrate to a large project the OZ dependency (dependecy conflicts.. I can't solve). 
+Therefore I extracted the important part of the code.
+
+
+I modified for this example the https://github.com/ivanminutillo/reagent-shadow-oz-example code.
+
+1. No issues with CLJSJS dependencies
+2. Works with any versions of Vega
+3. Few lines (Shadow-cljs)
+
+## Usage
+Clone the demo
+
+```
+git clone https://github.com/ivanminutillo/minime-oz-shadow-example.git
+```
+
+Enter the project and install the needed dependencies
+
+```
+cd minime-oz-shadow-example && yarn install
+```
+
+Start the app
+
+```
+shadow-cljs watch app
+
+```
+Visit http://localhost:3000
---
+++ src/app/vega.cljs
@@ -0,0 +1,35 @@
+(ns app.vega
+  (:require [reagent.core :as r]
+            ["vega" :as vegas]
+            ["vega-lite" :as vl]
+            ["vega-embed" :as ve]
+            ["vega-tooltip" :as vt]))
+
+
+(defn log [a-thing]
+  (.log js/console a-thing))
+
+
+(defn parse-vl-spec [spec elem]
+  (when spec
+    (let [opts #js {"mode" "vega-lite"
+                    "renderer" "canvas"
+                    "actions" false}]
+      (-> (ve elem spec opts)
+          (.then (fn [res]
+                   (log res)
+                   (. vt (vl (.-view res) spec))))
+          (.catch (fn [err]
+                    (log err)))))))
+
+(defn vega-lite
+  "Reagent component that renders vega-lite."
+  [spec]
+  (r/create-class
+   {:display-name "vega-lite"
+    :component-did-mount (fn [this]
+                           (parse-vl-spec spec (r/dom-node this)))
+    :component-will-update (fn [this [_ new-spec]]
+                             (parse-vl-spec new-spec (r/dom-node this)))
+    :reagent-render (fn [spec]
+                      [:div#vis])}))
